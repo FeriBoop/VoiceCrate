a4cf57d9828e9270bd60a6ac4396e2c6655ec67d
RokZerdoner <rok.zerdonern@gmail.com>
2024-12-16 15:06:01 +0100

    VC-12 Frontend form validation
    Added modal window for error and message displaying. Added frontend validation inside login and registration and implemented message displaying based on what server responded with

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -14,56 +14,115 @@ import {
   Checkbox,
 } from '@chakra-ui/react';
 import { UserContext, UserContextType } from '../userContext';
+import CustomModal from "../components/CustomModalProps";
 
 // TODO - Add validation for input fields
 // TODO - Display differend errors (from backend) for failed registration
 
+/**
+ * Login Function
+ * User must provide username and password
+ * There is validation on server side
+ */
 const Login: React.FC = () => {
+  //form data varables
   const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState<string>('');
+  const [usernameError, setUsernameError] = useState<string>('');
+  const [passwordError, setPasswordError] = useState<string>('');
   const [error, setError] = useState<string>('');
   const [showPassword, setShowPassword] = useState<boolean>(false);
 
+  //modal window variables
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+  const [modalTitle, setModalTitle] = useState<string>(''); // Title for modal
+  const [modalMessage, setModalMessage] = useState<string>('');
+  const [backgroundColor, setBackgroundColor] = useState<string>('');
+  const [headerColor, setHeaderColor] = useState<string>('');
+  const [bodyColor, setBodyColor] = useState<string>('');
+
+  //user session variables
   const userContext = useContext<UserContextType>(UserContext);
   const toast = useToast();
 
+  /**
+   * login function. It sends post request on API and handles the result from it
+   * @param e
+   */
   const handleLogin = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
-    try {
-      const res = await fetch('http://localhost:3000/user/login', {
-        method: 'POST',
-        //credentials: 'include',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password }),
-      });
-
-      if (!res.ok) {
-        throw new Error('Network response was not ok');
-      }
-
-      const data = await res.json();
-
-      if (data && data._id) {
-        userContext.setUserContext(data);
-        toast({
-          title: 'Prijava uspešna',
-          description: `Dobrodošli, ${username}!`,
-          status: 'success',
-          duration: 3000,
-          isClosable: true,
+    //check for empty string
+    if(username === ''){
+      setUsernameError('Uporabniško ime ne sme biti prazno!!!');
+    }
+    else if(password === ''){
+      setPasswordError('Geslo ne sme biti prazno!!!');
+    }
+    else {
+      //send post request to server
+      try {
+        const res = await fetch('http://localhost:3000/user/login', {
+          method: 'POST',
+          //credentials: 'include',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({username, password}),
         });
-      } else {
-        setError('Nepravilno uporabniško ime ali geslo');
+
+        const data = await res.json();
+
+        if (!res.ok) { // if result status is not 200
+          //throw new Error('Network response was not ok');
+          setModalTitle("Ups");
+          setModalMessage(data.message);
+          setIsModalOpen(true);
+          setBackgroundColor("red.200");
+          setHeaderColor("blue.600");
+          setBodyColor("blue.600");
+          setUsername('');
+          setPassword('');
+        }
+
+
+        if (data && data._id) { // if result status is 200
+          setModalTitle('Prijava uspešna');
+          setModalMessage(`Dobrodošli nazaj, ${username}!`);
+          setBackgroundColor("green.200");
+          setHeaderColor("blue.600");
+          setBodyColor("blue.600");
+          setIsModalOpen(true);
+
+          //navigate to index
+          setTimeout(() => {
+            setIsModalOpen(false); // Close modal
+            userContext.setUserContext(data); // Setting user context with the new user data
+          }, 1500);
+
+        } else { // if error
+          setModalTitle("Ups");
+          setModalMessage(data.message);
+          setIsModalOpen(true);
+          setBackgroundColor("red.200");
+          setHeaderColor("blue.600");
+          setBodyColor("blue.600");
+          setUsername('');
+          setPassword('');
+        }
+      } catch (error) { // if catch error
+        console.error(error);
+        setModalTitle('Napaka');
+        setModalMessage('Napaka pri prijavi. Poskusite znova.');
+        setIsModalOpen(true);
+        setBackgroundColor("red.200");
+        setHeaderColor("blue.600");
+        setBodyColor("blue.600");
         setUsername('');
         setPassword('');
       }
-    } catch (error) {
-      console.error(error);
-      setError('Napaka pri prijavi. Poskusite znova.');
     }
   };
 
+  //return render
   return (
     <Box
       maxW="xl"
@@ -103,6 +162,11 @@ const Login: React.FC = () => {
               onChange={(e) => setUsername(e.target.value)}
               size="lg"
             />
+            {usernameError !== '' &&
+                <Text color="red.500" fontSize="sm" textAlign="center">
+                  {usernameError}
+                </Text>
+            }
           </FormControl>
 
           <FormControl id="password" isRequired>
@@ -114,6 +178,11 @@ const Login: React.FC = () => {
               onChange={(e) => setPassword(e.target.value)}
               size="lg"
             />
+            {passwordError === '' &&
+                <Text color="red.500" fontSize="sm" textAlign="left">
+                  {usernameError}
+                </Text>
+            }
             <Checkbox
               mt={2}
               isChecked={showPassword}
@@ -141,7 +210,18 @@ const Login: React.FC = () => {
           </Button>
         </Stack>
       </form>
+      <CustomModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          title={modalTitle}
+          message={modalMessage}
+          backgroundColor={backgroundColor}
+          headerColor={headerColor}
+          bodyColor={bodyColor}
+          duration={1500}
+      />
     </Box>
+
   );
 };
 
